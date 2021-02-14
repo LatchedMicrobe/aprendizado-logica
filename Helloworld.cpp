963bd14837533a2231bec9e064d92514438f79e6
Rodrigo Pontes <78002168+LatchedMicrobe@users.noreply.github.com>
2021-02-14 01:38:42 -0300

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,6 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void){
-	printf("Hello World");
+int main(void)
+{
+	int var1;
+	printf("Enter a number");
+	scanf("%d", &var1);
+	
+	printf("The number you entered is: %d", var1);
+	
 	return 0;
 }

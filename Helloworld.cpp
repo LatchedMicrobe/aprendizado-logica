0ec5b02cbd4b923ed8cc1a703f4acde829524d9c
Rodrigo Pontes <78002168+LatchedMicrobe@users.noreply.github.com>
2021-02-14 01:55:58 -0300

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -4,10 +4,10 @@
 int main(void)
 {
 	int var1, var2;
-	printf("Enter a number");
+	printf("Enter a number: ");
 	scanf("%d", &var1);
 
-	printf("Enter a number");
+	printf("Enter a number: ");
 	scanf("%d", &var2);
 
 	printf("The sum is: %d", var1 + var2);
